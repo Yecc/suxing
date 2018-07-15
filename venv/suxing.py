e7f1a32d8db9fb492b58aaa2becd61a4a9caf965
yangsu <yangsuuu@outlook.com>
2018-07-15 21:56:31 +0800

    Initial commit

---
+++ venv/suxing.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+class G:
+    pageEnd = False
+
+def get_pages_id(page_num):
+    url = 'https://m.dianping.com/activity/static/list?page='+ str(page_num) +'&cityid=2&regionParentId=0&regionId=0&type=1&sort=0&filter=0'
+    r = requests.get(url)
+    return r.text
+
+def parser_pages(id_str):
+    id_dict = json.loads(id_str)
+    isend = id_dict['data']['pageEnd']
+
+    if isend:
+        G.pageEnd = True
+def main():
+    for i in range(1, 10):
+        parser_pages(get_pages_id(i))
+        if G.pageEnd:
+            break
+main()
